2545e25beae9a0d81dab99428ff141c8a517b867
Javier Irizarry-Delgado <92115922+Shxl2@users.noreply.github.com>
2024-06-18 09:17:23 -0400

    Changed to use temporal commands

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/BlueAutoCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/BlueAutoCommand.java
@@ -4,17 +4,23 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 
 import org.firstinspires.ftc.teamcode.subsystems.drive.DrivetrainSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.intake.IntakeSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.pivot.PivotSubsystem;
 
 public class BlueAutoCommand extends SequentialCommandGroup {
-    public BlueAutoCommand(DrivetrainSubsystem drive) {
+    public BlueAutoCommand(DrivetrainSubsystem drive, IntakeSubsystem intake, PivotSubsystem pivot) {
         addCommands(
                 new FollowTrajectoryCommand(drive, drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
                         // Add movements here
                         .forward(1)
                         .turn(Math.toRadians(90))
                         .strafeLeft(10)
+                        .addTemporalMarker(new PivotCommand(pivot, Math.toRadians(0))::schedule)
+                        .waitSeconds(10)
+                        .strafeRight(10)
                         .build()
                 )
+
         );
     }
 }
