04a5218ab47de55d33c45c41de713a9d24896162
RisingNinjas16391 <106979512+Team-2383@users.noreply.github.com>
2024-07-23 10:53:14 -0400

    session 4 base

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContainer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContainer.java
@@ -41,6 +41,7 @@ public class RobotContainer {
         m_outtakePosition = new GamepadButton(m_driverController, GamepadKeys.Button.RIGHT_BUMPER);
         m_intakePosition = new GamepadButton(m_driverController, GamepadKeys.Button.LEFT_BUMPER);
 
+
         m_resetHeading = new GamepadButton(m_driverController, GamepadKeys.Button.START);
 
         if (autoNum == 0) {
@@ -61,10 +62,11 @@ public class RobotContainer {
         m_driveSubsystem.setDefaultCommand(new TeleOpDriveCommand(
                 m_driveSubsystem, m_driverController::getLeftY,
                 m_driverController::getLeftX, m_driverController::getRightX));
+
         m_pivotSubsystem.setDefaultCommand(new PivotPowerCommand(
                 m_pivotSubsystem, () -> (m_driverController.getButton(GamepadKeys.Button.A) ? 1 : m_driverController.getButton(GamepadKeys.Button.B) ? -1 : 0)));
 
-        m_intakeSubsystem.setDefaultCommand(new IntakeCommand(m_intakeSubsystem, () -> m_driverController.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER) - m_driverController.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER)));
+        m_intakeSubsystem.setDefaultCommand(new IntakeCommand(m_intakeSubsystem, () -> m_driverController.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER) - m_driverController.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER)));
     }
 
     public void configureButtonBindings() {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/RedAutoCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/RedAutoCommand.java
@@ -3,14 +3,9 @@ package org.firstinspires.ftc.teamcode.commands;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 
-import org.firstinspires.ftc.teamcode.subsystems.claw.ClawSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.drive.DrivetrainSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.intake.IntakeSubsystem;
-import org.firstinspires.ftc.teamcode.commands.IntakeCommand;
 import org.firstinspires.ftc.teamcode.subsystems.pivot.PivotSubsystem;
-import org.firstinspires.ftc.teamcode.subsystems.shooter.ShooterSubsystem;
-import org.firstinspires.ftc.teamcode.subsystems.claw.ClawSubsystem;
-import org.firstinspires.ftc.teamcode.commands.ClawCommand;
 
 public class RedAutoCommand extends SequentialCommandGroup {
     public RedAutoCommand(DrivetrainSubsystem drive, IntakeSubsystem intake, PivotSubsystem pivot) {
