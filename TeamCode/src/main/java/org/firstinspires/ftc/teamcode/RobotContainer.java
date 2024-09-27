8f3b7bf360f93e653e12e424151e86611df85157
Anay Sharma <115898324+AnaySharma01@users.noreply.github.com>
2024-09-27 16:13:47 -0400

    Update RobotContainer.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContainer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContainer.java
@@ -22,24 +22,24 @@ import org.firstinspires.ftc.teamcode.subsystems.pivot.PivotSubsystem;
 public class RobotContainer {
     private final DrivetrainSubsystem m_driveSubsystem;
     private final PivotSubsystem m_pivotSubsystem;
-    private final IntakeSubsystem m_intakeSubsystem;
+    //private final IntakeSubsystem m_intakeSubsystem;
 
     private final GamepadEx m_driverController;
 
-    private final GamepadButton m_outtakePosition;
-    private final GamepadButton m_intakePosition;
+    //private final GamepadButton m_outtakePosition;
+   // private final GamepadButton m_intakePosition;
     private final GamepadButton m_resetHeading;
 
 
     public RobotContainer(HardwareMap hwMap, Gamepad gamepad1, Gamepad gamepad2, int autoNum){
         m_driveSubsystem = new DrivetrainSubsystem(hwMap, true);
         m_pivotSubsystem = new PivotSubsystem(hwMap);
-        m_intakeSubsystem = new IntakeSubsystem(hwMap);
+        //m_intakeSubsystem = new IntakeSubsystem(hwMap);
 
         m_driverController = new GamepadEx(gamepad1);
 
-        m_outtakePosition = new GamepadButton(m_driverController, GamepadKeys.Button.RIGHT_BUMPER);
-        m_intakePosition = new GamepadButton(m_driverController, GamepadKeys.Button.LEFT_BUMPER);
+       // m_outtakePosition = new GamepadButton(m_driverController, GamepadKeys.Button.RIGHT_BUMPER);
+       // m_intakePosition = new GamepadButton(m_driverController, GamepadKeys.Button.LEFT_BUMPER);
 
         m_resetHeading = new GamepadButton(m_driverController, GamepadKeys.Button.START);
 
@@ -64,12 +64,12 @@ public class RobotContainer {
         m_pivotSubsystem.setDefaultCommand(new PivotPowerCommand(
                 m_pivotSubsystem, () -> (m_driverController.getButton(GamepadKeys.Button.A) ? 1 : m_driverController.getButton(GamepadKeys.Button.B) ? -1 : 0)));
 
-        m_intakeSubsystem.setDefaultCommand(new IntakeCommand(m_intakeSubsystem, () -> m_driverController.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER) - m_driverController.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER)));
+       // m_intakeSubsystem.setDefaultCommand(new IntakeCommand(m_intakeSubsystem, () -> m_driverController.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER) - m_driverController.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER)));
     }
 
     public void configureButtonBindings() {
-        m_outtakePosition.whenPressed(new PivotCommand(m_pivotSubsystem, Math.toRadians(10)));
-        m_intakePosition.whenPressed(new PivotCommand(m_pivotSubsystem, Math.toRadians(-20)));
+       // m_outtakePosition.whenPressed(new PivotCommand(m_pivotSubsystem, Math.toRadians(10)));
+       // m_intakePosition.whenPressed(new PivotCommand(m_pivotSubsystem, Math.toRadians(-20)));
 
         m_resetHeading.whenPressed(new InstantCommand(m_driveSubsystem::resetHeading));
     }
